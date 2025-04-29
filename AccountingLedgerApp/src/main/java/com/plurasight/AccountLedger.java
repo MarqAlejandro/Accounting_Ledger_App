58159fd71e24e42382ec8e854ee7a080e9152cc8
MarqAlejandro <alejandromarq3@gmail.com>
2025-04-29 10:25:46 -0700

    all displayLedger methods working
    all methods under the ledger method display properly

--- AccountingLedgerApp/src/main/java/com/plurasight/AccountLedger.java
+++ AccountingLedgerApp/src/main/java/com/plurasight/AccountLedger.java
@@ -1,6 +1,7 @@
 package com.plurasight;
 
 import java.io.*;
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ information and save it to the csv file
     private List<Transaction> transactionList = new ArrayList<>();
 
 
-    public void homeScreen(){
+    public void homeScreen() {
         loadTransactionInfo();
 
         System.out.println("\nWelcome, please input one of the following: " +
@@ -33,25 +34,22 @@ information and save it to the csv file
                 "\n\tP) Make Payment (Debit)" +
                 "\n\tL) Ledger" +
                 "\n\tX) Exit");
-        
+
         int userInputHome = 0;
         String userInput = scanner.nextLine();
-        if(userInput.equalsIgnoreCase("d")){
+        if (userInput.equalsIgnoreCase("d")) {
             userInputHome = 1;
-        }
-        else if (userInput.equalsIgnoreCase("p")){
+        } else if (userInput.equalsIgnoreCase("p")) {
             userInputHome = 2;
-        }
-        else if (userInput.equalsIgnoreCase("l")){
+        } else if (userInput.equalsIgnoreCase("l")) {
             userInputHome = 3;
         } else if (userInput.equalsIgnoreCase("x")) {
             userInputHome = 4;
-        }
-        else{
+        } else {
             System.out.println("user input was not one of the options");
         }
-        
-        switch (userInputHome){
+
+        switch (userInputHome) {
             case 1:
                 System.out.println("working on addDeposit method");
 
@@ -71,37 +69,37 @@ information and save it to the csv file
         }
     }//end of homeScreen method
 
-    public void loadTransactionInfo(){                                                                          //method to load Transaction info from .csv file onto ArrayList
+    public void loadTransactionInfo() {                                                                          //method to load Transaction info from .csv file onto ArrayList
 
-             try {
-                 System.out.println("Loading Account Information");
+        try {
+            System.out.println("Loading Account Information");
 
-                 BufferedReader bufReader = new BufferedReader(new FileReader("transactions.csv"));     //BufferedReader variable that takes a FileReader as arguement that takes a .csv file arguement
-                 String FileInput;                                                                   //String Variable to hold transaction info
+            BufferedReader bufReader = new BufferedReader(new FileReader("transactions.csv"));     //BufferedReader variable that takes a FileReader as arguement that takes a .csv file arguement
+            String FileInput;                                                                   //String Variable to hold transaction info
 
-                 bufReader.readLine();                                                                       //skip the first line, assumes that the first line is headers and garbage data
+            bufReader.readLine();                                                                       //skip the first line, assumes that the first line is headers and garbage data
 
-                 while ((FileInput = bufReader.readLine()) != null) {                                //in the midst of while loop read a line from .csv file and load it onto String Variable and check if it comes out null
-                     String[] tokens = FileInput.split(Pattern.quote("|"));                   //load the line onto a String array so that it can be partitioned by the pattern "|"
-                     Transaction transaction = new Transaction();                                                 //create an empty Transaction object
-                     if(tokens.length == 5) {
-                         transaction.setRecordedDate(LocalDate.parse(tokens[0]));
-                         transaction.setRecordedTime(LocalTime.parse(tokens[1]));
-                         transaction.setDescription(tokens[2]);
-                         transaction.setVendor(tokens[3]);
-                         transaction.setAmount(Double.parseDouble(tokens[4]))                                  ;//will load and set all transaction information only if there is exactly 5 elements in the String Array
-                     }
-                     else{
-                         System.out.println("error: missing or too much information on a given transaction");
-                     }
-                     transactionList.add(transaction);                                                         //load the Employee object onto the ArrayList for Employee
-                 }
-                 bufReader.close();                                                                          //bufferedReader close
+            while ((FileInput = bufReader.readLine()) != null) {                                //in the midst of while loop read a line from .csv file and load it onto String Variable and check if it comes out null
+                String[] tokens = FileInput.split(Pattern.quote("|"));                   //load the line onto a String array so that it can be partitioned by the pattern "|"
+                Transaction transaction = new Transaction();                                                 //create an empty Transaction object
+                if (tokens.length == 5) {
+                    transaction.setRecordedDate(LocalDate.parse(tokens[0]));
+                    transaction.setRecordedTime(LocalTime.parse(tokens[1]));
+                    transaction.setDescription(tokens[2]);
+                    transaction.setVendor(tokens[3]);
+                    transaction.setAmount(Double.parseDouble(tokens[4]));//will load and set all transaction information only if there is exactly 5 elements in the String Array
+                } else {
+                    System.out.println("error: missing or too much information on a given transaction");
+                }
+                transactionList.add(transaction);                                                         //load the Employee object onto the ArrayList for Employee
+            }
+            bufReader.close();                                                                          //bufferedReader close
 
-             } catch (IOException e) {                                                                       //in case of an error with I/O
-                    System.out.println("error with .csv file naming, please check if its the correct save file");                              //catch to loop back to beginning
+        } catch (
+                IOException e) {                                                                       //in case of an error with I/O
+            System.out.println("error with .csv file naming, please check if its the correct save file");                              //catch to loop back to beginning
 
-             }
+        }
     }
 
          /*
@@ -118,7 +116,7 @@ reports or to run a custom search
 
           */
 
-    public void ledger(){
+    public void ledger() {
         System.out.println("\nLedger Display Screen, please input one of the following: " +
                 "\n\tA) All entries" +
                 "\n\tD) Deposits" +
@@ -128,37 +126,29 @@ reports or to run a custom search
 
         int userInputLedger = 0;
         String userInput = scanner.nextLine();
-        if(userInput.equalsIgnoreCase("a")){
+        if (userInput.equalsIgnoreCase("a")) {
             userInputLedger = 1;
-        }
-        else if (userInput.equalsIgnoreCase("d")){
+        } else if (userInput.equalsIgnoreCase("d")) {
             userInputLedger = 2;
-        }
-        else if (userInput.equalsIgnoreCase("p")){
+        } else if (userInput.equalsIgnoreCase("p")) {
             userInputLedger = 3;
         } else if (userInput.equalsIgnoreCase("r")) {
             userInputLedger = 4;
-        }
-        else if (userInput.equalsIgnoreCase("h")){
+        } else if (userInput.equalsIgnoreCase("h")) {
             userInputLedger = 5;
-        }
-        else{
+        } else {
             System.out.println("user input was not one of the options");
         }
 
-        switch (userInputLedger){
+        switch (userInputLedger) {
             case 1:
-                System.out.println("working on displayAll method");
-                break;
+                displayAll();
             case 2:
-                System.out.println("working on displayDepositsOnly method");
-                break;
+                displayDepositOnly();
             case 3:
-                System.out.println("working on displayPaymentsOnly method");
-                break;
+                displayPaymentsOnly();
             case 4:
                 reports();
-                break;
             case 5:
                 System.out.println("Returning to Home Page");
                 homeScreen();
@@ -166,6 +156,49 @@ reports or to run a custom search
                 System.out.println("Please try again - ledger");
                 ledger();
         }
+    }
+
+    public void displayAll() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+        System.out.println("Printing All Account Transaction Information:");
+        System.out.println();
+        for (Transaction transaction : transactionList) {                                                           //for-each loop to iterate through
+            String formatDate = transaction.getRecordedDate().format(dateFormatter);
+            String formatTime = transaction.getRecordedTime().format(timeFormatter);
+            System.out.printf("Date and Time: %s : %s |\t Descripton: %s |\t Vendor: %s |\t Amount: %.2f\n", formatDate, formatTime, transaction.getDescription(), transaction.getVendor(), transaction.getAmount());
+        }
+        ledger();
+    }
+
+    public void displayDepositOnly(){
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+        System.out.println("Printing All Deposits Information:");
+        System.out.println();
+        for (Transaction transaction : transactionList) {                                                           //for-each loop to iterate through
+            if(transaction.getAmount() > 0) {
+                String formatDate = transaction.getRecordedDate().format(dateFormatter);
+                String formatTime = transaction.getRecordedTime().format(timeFormatter);
+                System.out.printf("Date and Time: %s : %s |\t Descripton: %s |\t Vendor: %s |\t Amount: %.2f\n", formatDate, formatTime, transaction.getDescription(), transaction.getVendor(), transaction.getAmount());
+            }
+        }
+        ledger();
+    }
+
+    public void displayPaymentsOnly(){
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+        System.out.println("Printing All Payments Information:");
+        System.out.println();
+        for (Transaction transaction : transactionList) {                                                           //for-each loop to iterate through
+            if(transaction.getAmount() < 0) {
+                String formatDate = transaction.getRecordedDate().format(dateFormatter);
+                String formatTime = transaction.getRecordedTime().format(timeFormatter);
+                System.out.printf("Date and Time: %s : %s |\t Descripton: %s |\t Vendor: %s |\t Amount: %.2f\n", formatDate, formatTime, transaction.getDescription(), transaction.getVendor(), transaction.getAmount());
+            }
+        }
+        ledger();
     }
 
      /*
@@ -181,7 +214,7 @@ and display all entries for that vendor
 o H) Home - go back to the home page
      */
 
-    public void reports(){
+    public void reports() {
         System.out.println("\nReports Display Screen, please input one of the following: " +
                 "\n\t1) Month To Date" +
                 "\n\t2) Previous Month" +
@@ -189,30 +222,30 @@ o H) Home - go back to the home page
                 "\n\t4) Previous Year" +
                 "\n\t5) Search by Vendor" +
                 "\n\t0) Back to ledger");
-        try{
-        int userInputReport = scanner.nextInt();
-        scanner.nextLine();
+        try {
+            int userInputReport = scanner.nextInt();
+            scanner.nextLine();
 
-        switch (userInputReport) {
-            case 0:
-                ledger();
-            case 1:
-                System.out.println("working on displayMonthToDate method");
-                break;
-            case 2:
-                System.out.println("working on displayPreviousMonth method");
-                break;
-            case 3:
-                System.out.println("working on displayYearToDate method");
-                break;
-            case 4:
-                System.out.println("working on displayPreviousYear method");
-                break;
-            case 5:
-                System.out.println("working on searchByVendor method");
-            default:
-                System.out.println("Input was not one of the options. Please try again");
-                reports();
+            switch (userInputReport) {
+                case 0:
+                    ledger();
+                case 1:
+                    System.out.println("working on displayMonthToDate method");
+                    break;
+                case 2:
+                    System.out.println("working on displayPreviousMonth method");
+                    break;
+                case 3:
+                    System.out.println("working on displayYearToDate method");
+                    break;
+                case 4:
+                    System.out.println("working on displayPreviousYear method");
+                    break;
+                case 5:
+                    System.out.println("working on searchByVendor method");
+                default:
+                    System.out.println("Input was not one of the options. Please try again");
+                    reports();
             }
         } catch (InputMismatchException e) {
             System.out.println("A non-number was inputted please try again");
@@ -221,8 +254,11 @@ o H) Home - go back to the home page
         }
     }
 
-    public void exit(){
+
+
+    public void exit() {
         System.out.println("Exiting the application, please come again");
         System.exit(0);
     }
+
 }
